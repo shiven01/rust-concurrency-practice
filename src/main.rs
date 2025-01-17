a79e0b4d5d9bb5ebe115b5905112fc076eb9bb62
Shiven Shekar <shivenshekar01@gmail.com>
2025-01-16 19:00:14 -0700

    Basic multiple producer single consumer implementation

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,5 @@
-use std::thread;
-use std::time::Duration;
+use std::sync::mpsc;
 
 fn main() {
-    thread::spawn(|| {
-        for i in 1..10 {
-            println!("hi number {i} from the spawned thread!");
-            thread::sleep(Duration::from_millis(1));
-        }
-    });
-
-    for i in 1..5 {
-        println!("hi number {i} from the main thread!");
-        thread::sleep(Duration::from_millis(1));
-    }
+    let (tx, rx) = mpsc::channel();
 }
